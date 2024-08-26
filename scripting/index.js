5178686f08b6c672221ea523cd5a96e3013e802a
Robert Stomski <stomski.robert@gmail.com>
2024-08-26 09:58:13 -0600

    temp tiny app

---
+++ scripting/index.js
@@ -0,0 +1,12 @@
+// so this is the index script, it imports all my scripting.
+
+import * as testing from "./testing.js";
+import * as canvas from "./canvas.js";
+
+console.log("hello from the INDEX.JS");
+
+testing.test();
+canvas.canvasBuild();
+
+// const canvas = document.getElementById("canvas");
+// console.log("canvas>>>>", canvas);
---
+++ scripting/testing.js
@@ -0,0 +1,7 @@
+export function test() {
+  console.log("Top of test script");
+  const testElement = document.querySelector("#testingh2");
+  //   console.log(testElement, "<test element, pre change");
+  testElement.innerText = "inner text mod test script working.";
+  //   console.log(testElement, "<test element, post change");
+}
